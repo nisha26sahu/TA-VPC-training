dd1d56c4c6b5f77ca73c9fd2e0774d93cc9df668
Nisha Sahu <Nisha>
2022-07-21 13:48:50 +0530

    modified nat gateway

--- routes.tf
+++ routes.tf
@@ -16,7 +16,7 @@ resource "aws_route_table" "nat_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.nat-gw.id
+    gateway_id = aws_nat_gateway.nat-gw.id
   }
 
   tags = {
