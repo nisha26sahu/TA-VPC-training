837fb07084438a5e1b382af1a170ea014c91cb07
Nisha Sahu <Nisha>
2022-07-21 13:46:46 +0530

    modiofied route table

--- gateways.tf
+++ gateways.tf
@@ -6,7 +6,7 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
-resource "aws_nat_gateway" "nat_gw" {
+resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public.id
 
